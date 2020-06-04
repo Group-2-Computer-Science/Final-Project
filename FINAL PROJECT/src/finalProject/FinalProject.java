bc036a4b1066db977a2563bed5adf8e47c44cca9
Madina-Omarova <66267002+Madina-Omarova@users.noreply.github.com>
2020-06-04 12:52:20 -0400

    Update FinalProject.java

--- FINAL PROJECT/src/finalProject/FinalProject.java
+++ FINAL PROJECT/src/finalProject/FinalProject.java
@@ -3,7 +3,11 @@ package finalProject;
 public class FinalProject {
 
 	public static void main(String[] args) {
-	// Project 2 
+	double mex=121000000;
+	double mexrate=1.01;
+	double us=315000000;
+	double usrate=0.15;
+	int year=0;
 
 	}
 
