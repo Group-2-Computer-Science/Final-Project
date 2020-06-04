4a9aa104134cd1ac6b9e59a176351ab81b8cb6bf
isabellaanes <66266427+isabellaanes@users.noreply.github.com>
2020-06-04 13:52:34 -0400

    Update FinalProject.java

--- FINAL PROJECT/src/finalProject/FinalProject.java
+++ FINAL PROJECT/src/finalProject/FinalProject.java
@@ -3,33 +3,31 @@ package finalProject;
 public class FinalProject {
 
 	public static void main(String[] args) {
-		double Mexico, US, year; 
+		public static void main(String[] args) {
+	// Project 2 
+		double Mexico, US; 
 		double rateM, rateUS; 
-			
-			year = 1;
-			rateM = 1.01; 
-			rateUS = 0.15; 
+		int year; 
+		
+		//Declare all the variables
+			year = 0;
+			rateM = 0.0101; 
+			rateUS = 0.0015; 
 			Mexico = 121000000; 
 			US = 315000000; 
 			
-			System.out.println("The population of Mexico on year " + year + " is: " + Mexico);
-			System.out.println("The population of the United States on year " + year + " is: " + US);
+			while (US >= Mexico) {
+				year+=1;
+				
+			System.out.println("Year " + year);
+			System.out.println("The population of Mexico is: " + Mexico);
+			System.out.println("The population of the United States is: " + US + "\n");
 			
 			Mexico = Mexico + (Mexico * rateM); 
 			US = US - (US * rateUS); 
-			
-			while (US>Mexico) {
-				year+=1;
-				Mexico = Mexico + (Mexico * rateM); 
-				US = US - (US * rateUS);
-				if (Mexico>US) {
-					System.out.println("It took " + year + " years for the population of Mexico to exceed the population of United States");
-					
-				}
-				
-				}
-		
-
+						
+		}
+			System.out.println("It took " + year + " years for the population of Mexico to exceed the population of United States");
+	}
 	}
 
-}
